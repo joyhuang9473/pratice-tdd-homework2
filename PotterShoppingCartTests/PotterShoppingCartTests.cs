fc61fda34d4e2c916118be26d23c9b363503093f
IEUser <IEUser>
2016-03-16 06:14:11 -0700

    Initialize project files
    [NEW] PotterShoppingCart project
    [NEW] PotterShoppingCartTests project

---
+++ PotterShoppingCartTests/PotterShoppingCartTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass()]
+    public class PotterShoppingCartTests
+    {
+
+    }
+}
